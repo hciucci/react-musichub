038f69021e44adb0a6620e4a9d0d99e81f888268
Haden Ciucci <hciucci@Hadens-Macbook-Air>
2024-11-05 20:56:27 -0500

    music player

--- react-layout/src/components/MusicPlayer.js
+++ react-layout/src/components/MusicPlayer.js
@@ -1,14 +1,24 @@
 // MusicPlayer.js
-import React from 'react';
+import React, { useRef } from 'react';
 
 const MusicPlayer = ({ nowPlaying }) => {
+    const audioRef = useRef(null);
+
+    // Function to handle play action
+    const playAudio = () => {
+        if (audioRef.current) {
+            audioRef.current.play();
+        }
+    };
+
     return (
         <div id="music-player-section">
             <p id="now-playing-text">Now Playing: {nowPlaying.artist}</p>
-            <audio id="music-player" controls>
+            <audio ref={audioRef} id="music-player" controls>
                 <source src={nowPlaying.song} type="audio/mpeg" />
                 Your browser does not support the audio element.
             </audio>
+            <button onClick={playAudio} style={{ marginTop: '10px' }}>Play</button>
         </div>
     );
 };
