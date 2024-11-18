34f40e15709c4bb9dfa38e08bbe4e9b92a0335fe
hciucci <hadenmciucci@gmail.com>
2024-11-18 17:34:16 -0500

    changes to a bit of css

--- react-layout/src/Layout.js
+++ react-layout/src/Layout.js
@@ -3,13 +3,14 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import './css/Layout.css';
 
 const Layout = () => {
   return (
-    <div>
+    <div className="layout-container">
       <Header />
-      <main>
-        <Outlet /> {/* This is where page-specific content renders */}
+      <main className="main-content">
+        <Outlet />
       </main>
       <Footer />
     </div>
